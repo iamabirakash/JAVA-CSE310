9c63067a9e92336d14acbcdd8b8a8bee62ca00e5
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-01-29 00:38:10 +0530

    29 Jan

---
+++ UNIT 1/Lecture 7/Vishal Number/Vishal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+// You are using Java
+    public static void main(String[] args) {
+        //Type your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        boolean check = (2*a==b);
+        System.out.println(check);
+    }
+}
