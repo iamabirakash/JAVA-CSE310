19ecd8ce1c76485ba10499251d44defa02da9e7a
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-01-29 00:39:07 +0530

    29 Jan

---
+++ UNIT 1/Lecture 7/Emma Consecutive Weeks/Emma.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+// You are using Java
+    public static void main(String[] args) {
+        //Type your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
