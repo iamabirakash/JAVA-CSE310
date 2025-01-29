1f3cb3d3171a5c77dc9991716e6483c7ec23bdc6
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-01-29 15:02:40 +0530

    Input in Command Line

---
+++ Class/commandLineArgument.java
@@ -0,0 +1,8 @@
+class commandLineArgument {
+    public static void main(String args[]) {
+        System.out.println("Starting the code");
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println(a+b);
+    }
+}
