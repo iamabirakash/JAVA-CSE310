8ed8ab7d79316a52f69279360fc34cabde20647b
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-02-05 10:45:04 +0530

    Create Maria.java

---
+++ UNIT 1/STATIC/Q2_MARIA/Maria.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+// You are using Java
+public static void main(String[] args) {
+        //type your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.println("(a + b - c * d / a) % b = "+(a + b - c * d / a) % b);
+}}
