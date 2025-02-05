360d98a9d8d9c44a3096d9f6e56029e213bcf9d1
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-02-05 10:44:15 +0530

    Update Carlos.java

--- UNIT 1/STATIC/Q1_Carlos/Carlos.java
+++ UNIT 1/STATIC/Q1_Carlos/Carlos.java
@@ -1,13 +1,14 @@
-import java.util.*;
-
-class Main {
+import java.util.Scanner;
+public class Main {
 // You are using Java
 public static void main(String[] args) {
         //type your code here
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
-        System.out.println("(a + b - c * d / a) % b = "+(a + b - c * d / a) % b);
-}}
+        String DN = sc.nextLine();
+        String Status = sc.nextLine();
+        int MN = sc.nextInt();
+        System.out.println("Device Name: "+DN);
+        System.out.println("Status: "+Status);
+        System.out.println("Model Number: "+MN);
+    }
+}
