a1759271d4c3991273fcef8f79038bb4a640411c
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-02-15 00:46:04 +0530

    Feb 15

---
+++ UNIT 2/LECTURE 14/JANCY STUDENT/jANCY.java
@@ -0,0 +1,8 @@
+// You are using Java
+    public static void main(String[] args) {
+        //Type your code here
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(s.toLowerCase());
+        
+    }
